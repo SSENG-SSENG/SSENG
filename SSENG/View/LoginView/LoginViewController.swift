4085a4d70143a5655b37291db96f93b3dd2b8e1a
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-07-16 00:20:21 +0900

    ✨: 위치 권한 설정 및 테스트를 위한 로그인 버튼 구현

--- SSENG/View/LoginView/LoginViewController.swift
+++ SSENG/View/LoginView/LoginViewController.swift
@@ -9,7 +9,32 @@ import Then
 import UIKit
 
 class LoginViewController: UIViewController {
+  private let loginButton = UIButton().then {
+    $0.setTitle("로그인", for: .normal)
+    $0.setTitleColor(.main, for: .normal)
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    setupUI()
+    setupConstraints()
+
+    loginButton.addTarget(self, action: #selector(didTapLogin), for: .touchUpInside)
+  }
+
+  func setupUI() {
+    [loginButton].forEach(view.addSubview)
+  }
+
+  func setupConstraints() {
+    loginButton.snp.makeConstraints {
+      $0.centerX.centerY.equalToSuperview()
+    }
+  }
+
+  @objc func didTapLogin() {
+    let mapVC = MapViewController()
+    navigationController?.pushViewController(mapVC, animated: true)
   }
 }
--- SSENG/View/MapView/MapViewController.swift
+++ SSENG/View/MapView/MapViewController.swift
@@ -4,18 +4,24 @@
 //
 //  Created by 이태윤 on 7/15/25.
 //
-import MapKit
+import CoreLocation
 import NMapsMap
 import SnapKit
 import Then
 import UIKit
 
 class MapViewController: UIViewController {
+  // 위치 권한
+  let locationManager = CLLocationManager()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
     let mapView = NMFMapView(frame: view.frame)
     view.addSubview(mapView)
+    locationManager.requestWhenInUseAuthorization()
+    locationManager.startUpdatingLocation()
+    mapView.positionMode = .direction
   }
 }
 
