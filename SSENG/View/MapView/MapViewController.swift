81d4952bb69c3ff18412dd85d76bc36220a0b663
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-07-15 23:45:57 +0900

    ✨: 네이버 지도 API를 SPM으로 받아와서 mapView워주기 #1

--- SSENG/View/MapView/MapViewController.swift
+++ SSENG/View/MapView/MapViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 이태윤 on 7/15/25.
 //
 import MapKit
+import NMapsMap
 import SnapKit
 import Then
 import UIKit
@@ -12,5 +13,15 @@ import UIKit
 class MapViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    let mapView = NMFMapView(frame: view.frame)
+    view.addSubview(mapView)
   }
+}
+
+// TODO: - 지도 API 받아오지 못 했을 경우 에러처리
+
+@available(iOS 17.0, *)
+#Preview {
+  MapViewController()
 }
