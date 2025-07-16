bb7818b71e25b6a78599e50b290dcae44ae8f95b
Gwangyong <key4168@icloud.com>
2025-07-16 15:33:42 +0900

    ✨: MyPage 화면 TableView 뼈대 및 3개 섹션 구조 구현 #5 - UITableView를 .insetGrouped 스타일로 추가 - SnapKit으로 SafeArea 전체 제약 설정 - DataSource 연결 - 3개의 섹션 및 각 섹션 헤더 타이틀 임시 추가 - 각 셀에 섹션/행 정보 텍스트를 표시하여 시각적으로 확인

--- SSENG/View/MypageView/MypageViewcontroller.swift
+++ SSENG/View/MypageView/MypageViewcontroller.swift
@@ -9,7 +9,53 @@ import Then
 import UIKit
 
 class MypageViewcontroller: UIViewController {
+  private lazy var tableView = UITableView(frame: .zero, style: .insetGrouped).then {
+    $0.delegate = self
+    $0.dataSource = self
+    $0.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupUI()
+  }
+
+  private func setupUI() {
+    view.addSubview(tableView)
+
+    tableView.snp.makeConstraints {
+      $0.directionalEdges.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
+}
+
+extension MypageViewcontroller: UITableViewDelegate {}
+
+extension MypageViewcontroller: UITableViewDataSource {
+  // 섹션의 개수를 반환하는 메서드
+  func numberOfSections(in _: UITableView) -> Int {
+    3
+  }
+
+  // 섹션의 title을 반환하는 메서드
+  func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
+    if section == 0 {
+      return "1번째 섹션"
+    } else if section == 1 {
+      return "2번째 섹션"
+    } else if section == 2 {
+      return "3번째 섹션"
+    }
+    return nil
+  }
+
+  func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+    5
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = "Section: \(indexPath.section) Row: \(indexPath.row)"
+    return cell
   }
 }
