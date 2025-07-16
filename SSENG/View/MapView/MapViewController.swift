9cfdc66f6ee6c2ca1d40fe99d5b9fda54255a1e6
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-07-16 23:30:55 +0900

    🗑️: 킥보드 버튼 코드 삭제 #14

--- SSENG/View/MapView/MapViewController.swift
+++ SSENG/View/MapView/MapViewController.swift
@@ -19,20 +19,6 @@ class MapViewController: UIViewController {
   // 위치
   let locationManager = CLLocationManager()
 
-  // 킥보드 등록 버튼
-  private let addKickBoardButton = UIButton().then {
-    $0.setImage(UIImage(systemName: "plus"), for: .normal)
-    $0.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 22, weight: .medium), forImageIn: .normal)
-    $0.tintColor = .main
-    $0.backgroundColor = UIColor.white.withAlphaComponent(0.7)
-    $0.layer.cornerRadius = 12
-    $0.clipsToBounds = false
-    $0.layer.shadowColor = UIColor.black.cgColor
-    $0.layer.shadowOpacity = 0.2
-    $0.layer.shadowOffset = CGSize(width: 0, height: 2)
-    $0.layer.shadowRadius = 4
-  }
-
   // 마이페이지 버튼
   private let myPageButton = UIButton().then {
     $0.setImage(UIImage(systemName: "person"), for: .normal)
@@ -110,12 +96,11 @@ class MapViewController: UIViewController {
   }
 
   private func setupUI() {
-    [mapView, controlStackView, addKickBoardButton, myPageButton].forEach { view.addSubview($0) }
+    [mapView, controlStackView, myPageButton].forEach { view.addSubview($0) }
     [reloadButton, dividerView, locationButton].forEach { controlStackView.addArrangedSubview($0) }
   }
 
   private func setupButtonActions() {
-    addKickBoardButton.addTarget(self, action: #selector(didTabAddKickBoardButton), for: .touchUpInside)
     myPageButton.addTarget(self, action: #selector(didTabMyPageButton), for: .touchUpInside)
     reloadButton.addTarget(self, action: #selector(didTabReloadButton), for: .touchUpInside)
     locationButton.addTarget(self, action: #selector(didTapLocationButton), for: .touchUpInside)
@@ -137,21 +122,12 @@ class MapViewController: UIViewController {
       $0.height.equalTo(1)
     }
 
-    addKickBoardButton.snp.makeConstraints {
-      $0.leading.equalToSuperview().inset(20)
-      $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
-      $0.size.equalTo(50)
-    }
-
     myPageButton.snp.makeConstraints {
       $0.trailing.equalToSuperview().inset(20)
       $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
       $0.size.equalTo(50)
     }
   }
-
-  // 킥보드 등록 버튼 액션
-  @objc private func didTabAddKickBoardButton() {}
 
   // 마이페이지 버튼 액션
   @objc private func didTabMyPageButton() {
