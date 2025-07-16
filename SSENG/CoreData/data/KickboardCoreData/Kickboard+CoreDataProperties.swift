69e4e83541f36d6c2f93a55cfddf084c64e34231
Luca Park <qlife1146@gmail.com>
2025-07-16 17:27:47 +0900

    💡: 킥보드에 들어갈 데이터 추가(남은 배터리 표기)

--- SSENG/CoreData/data/KickboardCoreData/Kickboard+CoreDataProperties.swift
+++ SSENG/CoreData/data/KickboardCoreData/Kickboard+CoreDataProperties.swift
@@ -16,12 +16,12 @@ public extension Kickboard {
 
   @NSManaged var id: String? // 킥보드 고유값(UUID)
   @NSManaged var battery: Int // 킥보드 배터리 잔량(80)
+  @NSManaged var batteryTime: String // 킥보드 배터리 남은 시간("약 120 분")
   @NSManaged var registerDate: String? // 킥보드 등록 날짜("2025-01-01 12:00:00")
   @NSManaged var location: String? // 킥보드 위치("lat/lng")
   @NSManaged var detailLocation: String? // 킥보드 위치("약국 앞")
   @NSManaged var type: String? // 킥보드 타입("1", "2")
-  @NSManaged var isRented: Bool // 킥보드 대여 상태(true, false)
-  // true: 대여 불가(탑승자 있음), false: 대여 가능(탑승자 없음)
+  @NSManaged var isRented: Bool // 킥보드 대여 상태(true: 대여 불가, false: 대여 가능)
 }
 
 extension Kickboard: Identifiable {}
--- SSENG/CoreData/data/KickboardCoreData/KickboardRepository.swift
+++ SSENG/CoreData/data/KickboardCoreData/KickboardRepository.swift
@@ -11,13 +11,15 @@ final class KickboardRepository {
   private let context = CoreDataStack.shared.context
 
   // 킥보드 등록
-  func registKickboard(registerDate: String, location: String, type: String) {
+  func registKickboard(registerDate: String, location: String, detailLocation: String, type: String) {
     let kb = Kickboard(context: context)
     kb.id = UUID().uuidString
     kb.registerDate = registerDate
     kb.location = location
+    kb.detailLocation = detailLocation
     kb.type = type
-    kb.battery = Int.random(in: 50 ... 100) // 배터리 잔량 50~100 사이의 랜덤 값
+    kb.battery = Int.random(in: 80 ... 100) // 배터리 잔량 50~100 사이의 랜덤 값
+    kb.batteryTime = "약 \(round(Double(kb.battery)) * 1.2) 분"
     kb.isRented = false // 초기 상태는 대여 가능
 
     CoreDataStack.shared.saveContext()
@@ -39,11 +41,12 @@ final class KickboardRepository {
   }
 
   // 킥보드 반납
-  func returnKickboard(location: String, newLocation: String) {
+  func returnKickboard(location: String, newLocation: String, detailLocation: String) {
     guard let kb = readKickboard(by: location) else { return }
 
     kb.isRented = false
     kb.location = newLocation
+    kb.detailLocation = detailLocation
     CoreDataStack.shared.saveContext()
   }
 
