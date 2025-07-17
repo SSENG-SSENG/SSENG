05cc276792012f8d5870ddb0ab56dea5fc083037
SeorinLee <yms9074@gmail.com>
2025-07-17 16:49:05 +0900

    💡:배터리 잔량 20-100으로 수정

--- SSENG/CoreData/data/KickboardCoreData/KickboardRepository.swift
+++ SSENG/CoreData/data/KickboardCoreData/KickboardRepository.swift
@@ -19,7 +19,7 @@ final class KickboardRepository {
     kb.location = location
     kb.detailLocation = detailLocation
     kb.type = type
-    kb.battery = Int16(Int.random(in: 80 ... 100)) // 배터리 잔량 50~100 사이의 랜덤 값
+    kb.battery = Int16(Int.random(in: 20 ... 100)) // 배터리 잔량 50~100 사이의 랜덤 값
     kb.batteryTime = "약 \(round(Double(kb.battery)) * 1.2) 분"
     kb.isRented = false // 초기 상태는 대여 가능
 
