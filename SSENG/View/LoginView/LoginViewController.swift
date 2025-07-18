db582d8ea1f43d2ad1700d0d5fbd0ccd15c21116
Luca Park <qlife1146@gmail.com>
2025-07-18 21:18:33 +0900

    ✨: 화면 누르면 키보드 없애기

--- SSENG/View/LoginView/LoginViewController.swift
+++ SSENG/View/LoginView/LoginViewController.swift
@@ -9,6 +9,9 @@ import Then
 import UIKit
 
 class LoginViewController: UIViewController {
+  private var isAgreed = false
+  private let repository = UserRepository()
+
   private let appLogoImageView = UIImageView().then {
     $0.image = UIImage(named: "Logo")
     $0.contentMode = .scaleAspectFit
@@ -49,6 +52,21 @@ class LoginViewController: UIViewController {
     $0.returnKeyType = .next
   }
 
+  private let autoLoginAgreeCheckBox = UIButton(type: .custom).then {
+    $0.setImage(UIImage(systemName: "square"), for: .normal)
+    $0.setImage(UIImage(systemName: "checkmark.square"), for: .selected)
+    $0.tintColor = .main
+  }
+
+  private let autoLoginAgreeLabel = UILabel().then {
+    $0.text = "자동 로그인"
+    $0.font = .systemFont(ofSize: 18, weight: .medium)
+  }
+
+  private let autoLoginStackView = UIStackView().then {
+    $0.axis = .horizontal
+  }
+
   private let pwStackView = UIStackView().then {
     $0.axis = .vertical
     $0.spacing = 6
@@ -78,15 +96,21 @@ class LoginViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
-    setupUI()
-    setupConstraints()
-    setupBUttonActions()
-    updateLoginButtonState()
-    addTextFieldObsevers()
+    if UserDefaults.standard.string(forKey: "loggedUserID") != nil {
+      let mapVC = MapViewController()
+      navigationController?.pushViewController(mapVC, animated: true)
+    } else {
+      setupUI()
+      setupConstraints()
+      setupBUttonActions()
+      updateLoginButtonState()
+      addTextFieldObsevers()
+      dismissKeyboardController()
+    }
   }
 
   func setupUI() {
-    [appLogoImageView, idStackView, pwStackView, loginButton, signUpBUtton, debugButton].forEach {
+    [appLogoImageView, idStackView, pwStackView, autoLoginStackView, loginButton, signUpBUtton, debugButton].forEach {
       view.addSubview($0)
     }
 
@@ -96,6 +120,10 @@ class LoginViewController: UIViewController {
 
     [pwLabel, pwTextField].forEach {
       pwStackView.addArrangedSubview($0)
+    }
+
+    [autoLoginAgreeCheckBox, autoLoginAgreeLabel].forEach {
+      autoLoginStackView.addArrangedSubview($0)
     }
   }
 
@@ -129,8 +157,14 @@ class LoginViewController: UIViewController {
       $0.centerX.equalToSuperview()
     }
 
+    autoLoginStackView.setCustomSpacing(4, after: autoLoginAgreeCheckBox)
+    autoLoginStackView.snp.makeConstraints {
+      $0.top.equalTo(pwStackView.snp.bottom).offset(10)
+      $0.centerX.equalToSuperview()
+    }
+
     loginButton.snp.makeConstraints {
-      $0.top.equalTo(pwStackView.snp.bottom).offset(24)
+      $0.top.equalTo(autoLoginStackView.snp.bottom).offset(24)
       $0.leading.trailing.equalToSuperview().inset(padding)
       $0.height.equalTo(height)
       $0.centerX.equalToSuperview()
@@ -155,6 +189,7 @@ class LoginViewController: UIViewController {
     loginButton.addTarget(self, action: #selector(didTapLogin), for: .touchUpInside)
     signUpBUtton.addTarget(self, action: #selector(didTapSignUp), for: .touchUpInside)
     debugButton.addTarget(self, action: #selector(donttouchthis), for: .touchUpInside)
+    autoLoginAgreeCheckBox.addTarget(self, action: #selector(didTapAutoLoginAgree), for: .touchUpInside)
   }
 
   private func addTextFieldObsevers() {
@@ -170,6 +205,15 @@ class LoginViewController: UIViewController {
 
     loginButton.isEnabled = allVAlid
     loginButton.alpha = allVAlid ? 1.0 : 0.5
+  }
+
+  func createDefaultUser(id: String) {
+    if isAgreed {
+      if UserDefaults.standard.string(forKey: "loggedUserID") == nil {
+        UserDefaults.standard.set(id, forKey: "loggedUserID")
+        return
+      }
+    }
   }
 
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String)
@@ -201,16 +245,23 @@ class LoginViewController: UIViewController {
     alert.addAction(UIAlertAction(title: "확인", style: .default))
     vc.present(alert, animated: true)
   }
+  
+  func dismissKeyboardController() {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+    tapGesture.cancelsTouchesInView = false
+    view.addGestureRecognizer(tapGesture)
+  }
 
   @objc func didTapLogin() {
-    if UserRepository().readUser(by: idTextField.text ?? "id-xxxx")?.id != idTextField.text {
+    if repository.readUser(by: idTextField.text ?? "id-xxxx")?.id != idTextField.text {
       alertController(on: self, title: "아이디 오류", message: "해당 아이디가 존재하지 않습니다.")
     }
 
-    if UserRepository().readUser(by: idTextField.text ?? "pw-xxxx")?.password != pwTextField.text {
+    if repository.readUser(by: idTextField.text ?? "pw-xxxx")?.password != pwTextField.text {
       alertController(on: self, title: "비밀번호 오류", message: "비밀번호가 일치하지 않습니다.")
     }
 
+    createDefaultUser(id: idTextField.text ?? "no id")
     let mapVC = MapViewController()
     navigationController?.pushViewController(mapVC, animated: true)
   }
@@ -220,6 +271,15 @@ class LoginViewController: UIViewController {
     navigationController?.pushViewController(signUpVC, animated: true)
   }
 
+  @objc func didTapAutoLoginAgree(_ sender: UIButton) {
+    sender.isSelected.toggle()
+    isAgreed = sender.isSelected
+  }
+
+  @objc func dismissKeyboard() {
+    view.endEditing(true)
+  }
+  
   @objc func donttouchthis() {
     // CoreDataStack.shared.deleteAllData()
     print(UserRepository().readUser(by: idTextField.text ?? "no id")?.password ?? "비밀번호 없음?!")
