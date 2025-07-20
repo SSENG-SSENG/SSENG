0f77e2a8f89e90ee11b64262fef3719c2135a15f
Gwangyong <key4168@icloud.com>
2025-07-20 14:30:38 +0900

    🚨: 킥보드 이용 시간 표시에서 초 단위까지 계산되도록 수정 #62 - 기존 duration 값에 * 60 처리 제거 - rideHour, rideMin, rideSec를 초 단위 기준으로 계산 - (시, 분, 초) 구성 후 "1시간 1분 5초" 형태로 출력하도록 수정

--- SSENG/View/MypageView/Cell/KickboardHistoryCell.swift
+++ SSENG/View/MypageView/Cell/KickboardHistoryCell.swift
@@ -106,36 +106,41 @@ class KickboardHistoryCell: UITableViewCell {
       dateLabel.text = dateFormatter.string(from: startTime)
 
       // 시작 시간(startTime)부터 총 이용 시간(duration)을 더해서 종료 시간(endTime) 계산
-      let endTime = startTime.addingTimeInterval(TimeInterval(history.duration * 60))
+      let endTime = startTime.addingTimeInterval(TimeInterval(history.duration))
       /// TimeInterval이 Double랑 동일한 의미인데, 시간 계산 때 의도를 명확하게 하려고 관습적(?)으로 사용한다고 한다.
       /// "이건 시간이야"라는 명확한 의도전달을 위해. Double과 기능적 차이 x
       /// addingTimeInterval(_:)는 Date 타입에 초(second) 단위의 Double값을 더해서 새로운 Date를 반환하는 메서드.
       ///
       /// startTime = 2025-07-18 22:00:00 같은 Date타입.
-      /// duration = 30(분)
+      /// duration = 1800(초)
       /// -> endTime = startTime + 1800초(30분)
       /// -> endTime = 2025-07-18 22:30:00 (자동 계산됨)
 
       let timeFormatter = DateFormatter()
       timeFormatter.locale = Locale(identifier: "ko_KR")
-      timeFormatter.dateFormat = "HH시mm분"
+      timeFormatter.dateFormat = "HH시mm분ss초"
 
       let timeRange = "\(timeFormatter.string(from: startTime)) ~ \(timeFormatter.string(from: endTime))" // "22시02분 ~ 22시43분" 형태로 생셩
 
-      let durationHour = history.duration / 60 // 시간 분리 (90 / 60 -> 1시간)
-      let durationMin = history.duration % 60 // 분 분리 (90 % 60 -> 30분)
+      let rideDuration = history.duration // 초 단위 (3665초)
+      let rideHour = rideDuration / 3600
+      let rideMin = (rideDuration % 3600) / 60
+      let rideSec = rideDuration % 60
 
-      // 시간, 분 조합해서 "1시간" or "35분" or "1시간 35분" 이런 형식으로 사용
-      let durationString: String
-      if durationMin == 0 {
-        durationString = "\(durationHour)시간"
-      } else if durationHour == 0 {
-        durationString = "\(durationMin)분"
-      } else {
-        durationString = "(\(durationHour)시간 \(durationMin)분)"
+      var totalDurations: [String] = []
+      if rideHour > 0 {
+        totalDurations.append("\(rideHour)시간")
+      }
+      if rideMin > 0 {
+        totalDurations.append("\(rideMin)분")
+      }
+      if rideSec > 0 {
+        totalDurations.append("\(rideSec)초")
       }
 
-      // 최종!! "22시02분 ~ 22시43분 (41분)" 으로 표시됨!!
+      let durationString = totalDurations.joined(separator: " ")
+
+      // 최종!! "22시00분00초 ~ 23시01분05초 (1시간1분5초)" 으로 표시됨!!
       timeRangeLabel.text = "\(timeRange) (\(durationString))"
     }
   }
