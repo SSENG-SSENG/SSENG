7254fc21c260c51f0897a8a83209af0c2f1edf78
Gwangyong <key4168@icloud.com>
2025-07-21 14:03:38 +0900

    ✨: 마이페이지 섹션 기본 접힘 및 킥보드 개수 표시 #84 - 등록한 킥보드 섹션 접힘 상태로 초기화 - viewForHeaderInSection에서 킥보드 개수 동적 표시 - 섹션 펼침 여부는 앱 재시작 시 초기화

--- SSENG/View/MypageView/MypageViewController.swift
+++ SSENG/View/MypageView/MypageViewController.swift
@@ -19,7 +19,7 @@ class MypageViewController: UIViewController {
   }
 
   private var section1ToggleButton: UIButton?
-  private var isKickboardSectionExpanded = true // 나중에 삭제 예정
+  private var isKickboardSectionExpanded = false
 
   private var user: User? // CoreData에서 가져온 유저 정보
   private let userRepository = UserRepository() // User 정보 가져올 때 사용
@@ -170,9 +170,12 @@ extension MypageViewController: UITableViewDataSource {
 
   // 각 섹션에 맞는 헤더 타이틀 설정
   func tableView(_: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    let registeredCount = kickboards.count
+    // 팀원과 상의하여 "등록한 킥보드"는 개수까지, "킥보드 이용 내역"은 개수를 제거하기로 결정
+
     let title: String
     switch section {
-    case 1: title = "등록한 킥보드"
+    case 1: title = "등록한 킥보드 (\(registeredCount)개)"
     case 2: title = "킥보드 이용 내역"
     default: return nil
     }
@@ -260,6 +263,8 @@ extension MypageViewController: UITableViewDataSource {
     }
   }
 }
+
+// MARK: - 킥보드 상태 이상 신고 Alert
 
 extension MypageViewController: KickboardHistoryCellDelegate {
   func didTapReportButton(_: KickboardHistoryCell) {
